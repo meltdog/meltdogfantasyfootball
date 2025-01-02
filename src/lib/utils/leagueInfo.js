272bf7ff06745a1dfe756a7dd0b750c0e1aeeb66
meltdog <calebcmelton@gmail.com>
2025-01-01 21:46:04 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1180084055259652096"; // your league ID
+export const leagueID = "1049046969282408448"; // your league ID
 export const leagueName = "MELTDOG FANTASY FOOTBALL"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
